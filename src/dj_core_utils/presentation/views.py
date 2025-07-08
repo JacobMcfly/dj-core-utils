93910127169b1012fe18e42cd2089166e1d1bd6c
Joaquín Lugo <joaquinlugo123@gmail.com>
2025-07-08 16:38:59 -0700

    change generic view names

--- src/dj_core_utils/presentation/views.py
+++ src/dj_core_utils/presentation/views.py
@@ -9,12 +9,12 @@ from dj_core_utils.presentation.serializers import ContentTypeSerializer, UserSe
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
-def obtener_content_type(request, app_label=None, model=None):
+def get_content_type(request, app_label=None, model=None):
     obj = get_object_or_404(ContentType, app_label=app_label, model=model)
     return Response(ContentTypeSerializer(obj).data)
 
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
-def obtener_mis_datos(request):
+def get_my_data(request):
     return Response(UserSerializer(request.user).data)
